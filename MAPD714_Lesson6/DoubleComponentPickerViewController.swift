30e3a87ca7685ba2f535e6ee5c3dc58c2758328e
Student <student@SETASs-Mac-mini.local>
2017-10-11 21:38:48 -0400

    Completed Double Picker view controller

--- MAPD714_Lesson6/DoubleComponentPickerViewController.swift
+++ MAPD714_Lesson6/DoubleComponentPickerViewController.swift
@@ -8,8 +8,13 @@
 
 import UIKit
 
-class DoubleComponentPickerViewController: UIViewController {
+class DoubleComponentPickerViewController: UIViewController,  UIPickerViewDelegate, UIPickerViewDataSource {
 
+    @IBOutlet weak var doublePicker: UIPickerView!
+    private let fillingComponent = 0
+    private let breadComponent = 1
+    private let fillingTypes = ["Turkey", "Ham", "Steak", "Fish", "Veggie"]
+    private let breadTypes = ["Brown", "White", "Whole Wheat", "Raisin"]
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -17,5 +22,44 @@ class DoubleComponentPickerViewController: UIViewController {
     }
 
 
+    @IBAction func onButtonPressed(_ sender: UIButton) {
+        let fillingRow = doublePicker.selectedRow(inComponent: fillingComponent)
+        let breadRow = doublePicker.selectedRow(inComponent: breadComponent)
+        let filling = fillingTypes[fillingRow]
+        let bread = breadTypes[breadRow]
+        let message = "Your \(filling) on \(bread) bread will be right up!"
+        
+        let alert = UIAlertController(
+            title: "Thank You for ordering",
+            message: message,
+            preferredStyle: .alert
+        )
+        
+        let action = UIAlertAction(
+            title: "Great!",
+            style: .default,
+            handler: nil)
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
 
+    
+    }
+
+    
+    //Data Source
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+      return 2
+    }
+    
+    //Data Source
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return (component == breadComponent) ? breadTypes.count : fillingTypes.count
+    }
+    
+    //Data Delegate
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return (component == breadComponent) ? breadTypes[row] : fillingTypes[row]
+    }
+    
 }
